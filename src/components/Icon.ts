b378d43a2ed430493c41c781c4d3d19bb36231ca
NetherMC <thelio.trinite@outlook.com>
2023-01-29 13:10:04 +0100

    icons

--- src/components/Icon.ts
+++ src/components/Icon.ts
@@ -2,24 +2,31 @@ import Component from './Component';
 
 export default class Icon extends Component {
 
-    connectedCallback() {
-        const icon = this.getAttribute('icon');
-        const width = this.getOptionalAttribute('width', 24);
-        const height = this.getOptionalAttribute('height', 24);
-        const tag = new DOMParser().parseFromString(this.outerHTML, 'text/html').querySelector('svg-icon');
-        let html = `<svg width="${width}px" height="${height}px" viewBox="0 0 24 24" class="${tag?.getAttribute('class')}">`;
-        switch (icon) {
-            case 'reply':
-                html += `<path d="M10 8.26667V4L3 11.4667L10 18.9333V14.56C15 14.56 18.5 16.2667 21 20C20 14.6667 17 9.33333 10 8.26667Z" fill="currentColor" />`;
-                break;
-            case 'id':
-                html += `<path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M5 2C3.34315 2 2 3.34315 2 5V19C2 20.6569 3.34315 22 5 22H19C20.6569 22 22 20.6569 22 19V5C22 3.34315 20.6569 2 19 2H5ZM8.79741 7.72V16H6.74541V7.72H8.79741ZM13.2097 7.72C16.0897 7.72 17.5897 9.388 17.5897 11.848C17.5897 14.308 16.0537 16 13.2577 16H10.3537V7.72H13.2097ZM13.1497 14.404C14.6137 14.404 15.5257 13.636 15.5257 11.86C15.5257 10.12 14.5537 9.316 13.1497 9.316H12.4057V14.404H13.1497Z" />`;
-                break;
-            default:
-                break;
-        }
-        html += '</svg>';
-        this.outerHTML = html;
+  connectedCallback() {
+    const icon = this.getAttribute('icon');
+    const width = this.getOptionalAttribute('width', 24);
+    const height = this.getOptionalAttribute('height', 24);
+    const tag = new DOMParser().parseFromString(this.outerHTML, 'text/html').querySelector('svg-icon');
+    let html = `<svg width="${width}px" height="${height}px" viewBox="0 0 ${width} ${height}" class="${tag?.getAttribute('class')}">`;
+    switch (icon) {
+      case 'reply':
+        html += `<path d="M10 8.26667V4L3 11.4667L10 18.9333V14.56C15 14.56 18.5 16.2667 21 20C20 14.6667 17 9.33333 10 8.26667Z" fill="currentColor" />`;
+        break;
+      case 'id':
+        html += `<path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M5 2C3.34315 2 2 3.34315 2 5V19C2 20.6569 3.34315 22 5 22H19C20.6569 22 22 20.6569 22 19V5C22 3.34315 20.6569 2 19 2H5ZM8.79741 7.72V16H6.74541V7.72H8.79741ZM13.2097 7.72C16.0897 7.72 17.5897 9.388 17.5897 11.848C17.5897 14.308 16.0537 16 13.2577 16H10.3537V7.72H13.2097ZM13.1497 14.404C14.6137 14.404 15.5257 13.636 15.5257 11.86C15.5257 10.12 14.5537 9.316 13.1497 9.316H12.4057V14.404H13.1497Z" />`;
+        break;
+      case 'download':
+        html += `<path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M16.293 9.293L17.707 10.707L12 16.414L6.29297 10.707L7.70697 9.293L11 12.586V2H13V12.586L16.293 9.293ZM18 20V18H20V20C20 21.102 19.104 22 18 22H6C4.896 22 4 21.102 4 20V18H6V20H18Z" />`;
+        break;
+      case 'unknown_file':
+        html += `<path d="m72 29.3v60.3c0 2.24 0 3.36-.44 4.22-.38.74-1 1.36-1.74 1.74-.86.44-1.98.44-4.22.44h-59.2c-2.24 0-3.36 0-4.22-.44-.74-.38-1.36-1-1.74-1.74-.44-.86-.44-1.98-.44-4.22v-83.2c0-2.24 0-3.36.44-4.22.38-.74 1-1.36 1.74-1.74.86-.44 1.98-.44 4.22-.44h36.3c1.96 0 2.94 0 3.86.22.5.12.98.28 1.44.5v16.88c0 2.24 0 3.36.44 4.22.38.74 1 1.36 1.74 1.74.86.44 1.98.44 4.22.44h16.88c.22.46.38.94.5 1.44.22.92.22 1.9.22 3.86z" fill="#d3d6fd" />
+                 <path d="m68.26 20.26c1.38 1.38 2.06 2.06 2.56 2.88.18.28.32.56.46.86h-16.88c-2.24 0-3.36 0-4.22-.44-.74-.38-1.36-1-1.74-1.74-.44-.86-.44-1.98-.44-4.22v-16.880029c.3.14.58.28.86.459999.82.5 1.5 1.18 2.88 2.56z" fill="#939bf9" />`;
+        break;
+      default:
+        break;
     }
+    html += '</svg>';
+    this.outerHTML = html;
+  }
 
 }
