f198b2b457710e5a7d102b23d580493fb0af74a0
NetherMC <thelio.trinite@outlook.com>
2023-01-29 15:53:20 +0100

    locale

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import ChannelsList from './components/Channels/List';
 import DMsList from './components/DMs/List';
 import { navigateTo } from './helpers/navigation';
 import { urlParts } from './helpers/url';
-import { guildChannels, tokenTest } from './helpers/api';
+import { guildChannels, tokenTest, User } from './helpers/api';
 import GuildsList from './components/Guilds/List';
 import { css } from '@emotion/css';
 import ChannelMessages from './components/Channels/Messages';
@@ -18,6 +18,7 @@ import MessageAction from './components/Message/Action';
 import { copyText } from './helpers/text';
 import MessageAttachment from './components/Message/Attachment';
 import MessageEmbed from './components/Message/Embed';
+import moment from 'moment';
 
 customElements.define('channels-list', ChannelsList);
 customElements.define('dms-list', DMsList);
@@ -37,90 +38,113 @@ const path = window.location.pathname;
 const root = document.querySelector<HTMLDivElement>('#app');
 if (!root) throw new Error('Cannot render app because the app root element cannot be found.');
 const pathRegexps = {
-    guild: /\/channels\/([0-9]{18,19})/gi,
-    channel: /\/channels\/([0-9]{18,19})\/([0-9]{18,19})/gi,
-    dm: /\/channels\/@me\/([0-9]{18,19})/gi,
+  guild: /\/channels\/([0-9]{18,19})/gi,
+  channel: /\/channels\/([0-9]{18,19})\/([0-9]{18,19})/gi,
+  dm: /\/channels\/@me\/([0-9]{18,19})/gi,
 };
 
+if (localStorage.getItem('locale') !== null) { // @ts-ignore
+  moment.locale(localStorage.getItem('locale'));
+  document.querySelector('html')?.setAttribute('lang', localStorage.getItem('locale'));
+}
+
 (async () => {
-    const guildId = urlParts()[1];
-    const token = new URLSearchParams(window.location.search).get('token');
-    if (path === '/login') {
-        if (token !== null) {
-            tokenTest(token).then(status => {
-                if (status === 200) {
-                    localStorage.setItem('token', token);
-                    navigateTo('/app');
-                }
-            });
+  const guildId = urlParts()[1];
+  const token = new URLSearchParams(window.location.search).get('token');
+
+  if (path === '/login') {
+    if (token !== null) {
+      tokenTest(token).then(async req => {
+        if (req.status === 200) {
+          const user: User = await req.json();
+          localStorage.setItem('token', token);
+          localStorage.setItem('locale', user.locale);
+          navigateTo('/app');
         }
-    } else if (path === '/app') {
-        navigateTo('/channels/@me');
-    } else if (path === '/channels/@me') {
-        root.innerHTML = `
-        <div class="${css({ display: 'flex' })}">
+      });
+    }
+  } else if (path === '/app') {
+    navigateTo('/channels/@me');
+  } else if (path === '/channels/@me') {
+    root.innerHTML = `
+        <div class="${css({display: 'flex'})}">
             <div class="${css({
-                backgroundColor: '#202225', overflow: 'hidden scroll', height: '100vh', width: '85px', maxWidth: '85px', minWidth: '85px'
-            })}">
-                <div class="${css({ height: '3px', backgroundColor: '#373a3f', marginLeft: '7px', marginRight: '7px' })}"></div>
+      backgroundColor: '#202225',
+      overflow: 'hidden scroll',
+      height: '100vh',
+      width: '85px',
+      maxWidth: '85px',
+      minWidth: '85px'
+    })}">
+                <div class="${css({height: '3px', backgroundColor: '#373a3f', marginLeft: '7px', marginRight: '7px'})}"></div>
                 <guilds-list></guilds-list>
                 <guilds-list-button button="join-guild"></guilds-list-button>
                 <guilds-list-button button="discovery"></guilds-list-button>
             </div>
-            <div class="${css({ display: 'flex', flexDirection: 'column', width: '100%' })}">
-                <div class="${css({ display: 'flex' })}">
+            <div class="${css({display: 'flex', flexDirection: 'column', width: '100%'})}">
+                <div class="${css({display: 'flex'})}">
                     <dms-list></dms-list>
                 </div>
             </div>
         </div>
         `;
-    } else if (pathRegexps.dm.test(path) && typeof urlParts()[2] !== 'undefined') {
-        root.innerHTML = `
-        <div class="${css({ display: 'flex' })}">
+  } else if (pathRegexps.dm.test(path) && typeof urlParts()[2] !== 'undefined') {
+    root.innerHTML = `
+        <div class="${css({display: 'flex'})}">
             <div class="${css({
-                backgroundColor: '#202225', overflow: 'hidden scroll', height: '100vh', width: '85px', maxWidth: '85px', minWidth: '85px'
-            })}">
-                <div class="${css({ height: '3px', backgroundColor: '#373a3f', marginLeft: '7px', marginRight: '7px' })}"></div>
+      backgroundColor: '#202225',
+      overflow: 'hidden scroll',
+      height: '100vh',
+      width: '85px',
+      maxWidth: '85px',
+      minWidth: '85px'
+    })}">
+                <div class="${css({height: '3px', backgroundColor: '#373a3f', marginLeft: '7px', marginRight: '7px'})}"></div>
                 <guilds-list></guilds-list>
                 <guilds-list-button button="join-guild"></guilds-list-button>
                 <guilds-list-button button="discovery"></guilds-list-button>
             </div>
-            <div class="${css({ display: 'flex', flexDirection: 'column', width: '100%' })}">
-                <div class="${css({ display: 'flex' })}">
+            <div class="${css({display: 'flex', flexDirection: 'column', width: '100%'})}">
+                <div class="${css({display: 'flex'})}">
                     <dms-list></dms-list>
                     <channel-messages></channel-messages>
                 </div>
             </div>
         </div>
         `;
-    } else if (pathRegexps.channel.test(path) && typeof urlParts()[2] !== 'undefined') {
-        root.innerHTML = `
-        <div class="${css({ display: 'flex' })}">
+  } else if (pathRegexps.channel.test(path) && typeof urlParts()[2] !== 'undefined') {
+    root.innerHTML = `
+        <div class="${css({display: 'flex'})}">
             <div class="${css({
-                backgroundColor: '#202225', overflow: 'hidden scroll', height: '100vh', width: '85px', maxWidth: '85px', minWidth: '85px'
-            })}">
-                <div class="${css({ height: '3px', backgroundColor: '#373a3f', marginLeft: '7px', marginRight: '7px' })}"></div>
+      backgroundColor: '#202225',
+      overflow: 'hidden scroll',
+      height: '100vh',
+      width: '85px',
+      maxWidth: '85px',
+      minWidth: '85px'
+    })}">
+                <div class="${css({height: '3px', backgroundColor: '#373a3f', marginLeft: '7px', marginRight: '7px'})}"></div>
                 <guilds-list></guilds-list>
                 <guilds-list-button button="join-guild"></guilds-list-button>
                 <guilds-list-button button="discovery"></guilds-list-button>
             </div>
-            <div class="${css({ display: 'flex', flexDirection: 'column', width: '100%' })}">
-                <div class="${css({ display: 'flex' })}">
+            <div class="${css({display: 'flex', flexDirection: 'column', width: '100%'})}">
+                <div class="${css({display: 'flex'})}">
                     <channels-list guild-id="${guildId}"></channels-list>
                     <channel-messages></channel-messages>
                 </div>
             </div>
         </div>
         `;
-    } else if (pathRegexps.guild.test(path)) {
-        const firstChannel = (await guildChannels(guildId))?.find(ch => ch.type !== 4 && ch.type !== 2)?.id;
-        navigateTo(`/channels/${guildId}/${firstChannel}`);
-    }
+  } else if (pathRegexps.guild.test(path)) {
+    const firstChannel = (await guildChannels(guildId))?.find(ch => ch.type !== 4 && ch.type !== 2)?.id;
+    navigateTo(`/channels/${guildId}/${firstChannel}`);
+  }
 })();
 
 // @ts-ignore
 window.copyText = copyText;
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js');
+  navigator.serviceWorker.register('/sw.js');
 }
