d2fea5e6413b13ac5b7ee34642a3c29150d782aa
NetherMC <thelio.trinite@outlook.com>
2023-02-03 17:15:10 +0100

    tabs + funcs

--- src/helpers/image.ts
+++ src/helpers/image.ts
@@ -1,28 +1,38 @@
 import { ArrayCSSInterpolation, ComponentSelector, css, CSSObject } from '@emotion/css';
 import { SerializedStyles } from '@emotion/utils';
+import { Emoji } from './api';
 
 export function defaultAvatarURL(discriminator: number, size?: number) {
-    return `https://cdn.discordapp.com/embed/avatars/${discriminator % 5}.png${size ? `?size=${size}` : ''}`;
+  return `https://cdn.discordapp.com/embed/avatars/${discriminator % 5}.png${size ? `?size=${size}` : ''}`;
 }
 
 export function avatarURL(id: number, avatar: string, discriminator: number, size?: number) {
-    return avatar ?
-        `https://cdn.discordapp.com/avatars/${id}/${avatar}.png${size ? `?size=${size}` : ''}` :
-        defaultAvatarURL(discriminator, size);
+  return avatar ?
+    `https://cdn.discordapp.com/avatars/${id}/${avatar}.png${size ? `?size=${size}` : ''}` :
+    defaultAvatarURL(discriminator, size);
 }
 
 export function groupAvatarURL(id: number, icon: string, size?: number) {
-    return icon ?
-        `https://cdn.discordapp.com/channel-icons/${id}/${icon}.png${size ? `?size=${size}` : ''}` : // assets/f7e38ac976a2a696161c923502a8345b.png
-        defaultAvatarURL(0, size);
+  return icon ?
+    `https://cdn.discordapp.com/channel-icons/${id}/${icon}.png${size ? `?size=${size}` : ''}` : // assets/f7e38ac976a2a696161c923502a8345b.png
+    defaultAvatarURL(0, size);
 }
 
 export function avatarImgHTML(url: string, sizes?: number, customCss?: ComponentSelector | SerializedStyles | CSSObject | ArrayCSSInterpolation) {
-    return `<img src="${url}" draggable="false" alt="" class="${css({
-        borderRadius: '9999px', width: sizes || '48px', height: sizes || '48px'
-    }, customCss)}" />`;
+  return `<img src="${url}" draggable="false" alt="" class="${css({
+    borderRadius: '9999px', width: sizes || '48px', height: sizes || '48px'
+  }, customCss)}" />`;
 }
 
-export function emojiURL(id: number, animated: boolean, size: number = 16) {
-    return `https://cdn.discordapp.com/emojis/${id}.${animated ? 'gif' : 'png'}${size ? `?size=${size}` : ''}`;
+export function emojiURL(id: number | string, animated: boolean, size: number = 16, quality: Emoji['quality']) {
+  const params = new URLSearchParams();
+  params.append('size', size.toString());
+  params.append('quality', quality);
+  return `https://cdn.discordapp.com/emojis/${id}.${animated ? 'gif' : 'png'}?${params}`;
+}
+
+export function guildIconURL(id: number | string, icon: string, animated: boolean, size: number = 48) {
+  const params = new URLSearchParams();
+  params.append('size', size.toString());
+  return `https://cdn.discordapp.com/icons/${id}/${icon}.${animated ? 'gif' : 'png'}?${params}`;
 }
