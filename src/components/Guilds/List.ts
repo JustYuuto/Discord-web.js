6d4b9796bcbbb3ed09cf7800d4499f0e5076ebda
NetherMC <thelio.trinite@outlook.com>
2023-02-03 17:14:53 +0100

    tabs + guild icon url

--- src/components/Guilds/List.ts
+++ src/components/Guilds/List.ts
@@ -1,35 +1,34 @@
 import { guilds } from '../../helpers/api';
 import { css } from '@emotion/css';
+import { guildIconURL } from '../../helpers/image';
 
 export default class GuildsList extends HTMLElement {
 
-    connectedCallback() {
-        guilds().then(guilds => {
-            let html = '';
-            guilds.forEach(guild => {
-                const iconCss = css({
-                    borderRadius: '50%', userSelect: 'none', transition: '.2s', ':hover': { borderRadius: '.75rem' },
-                    width: '48px', height: '48px'
-                });
-                html += `<div class="${css({ marginTop: '4px', marginBottom: '4px' })}">`;
-                html += `<a href="/channels/${guild.id}" class="${css({ ':hover': { textDecoration: 'none' } })}">`;
-                if (!!guild.icon) {
-                    html += `<img src="https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png?size=48" 
-                                  alt="${guild.name}" class="${iconCss}" />`;
-                } else {
-                    let iconText = '';
-                    guild.name.split(' ').forEach(w => iconText += w[0]);
-                    html += `<span class="${css([iconCss, {
-                        color: 'white', width: '48px', height: '48px', display: 'flex', alignItems: 'center', 
-                        justifyContent: 'center', backgroundColor: '#36393f', marginBottom: '6px',
-                        ':hover': { backgroundColor: '#5865f2' }
-                    }])}">${iconText}</span>`;
-                }
-                html += '</a></div>';
-            });
-            this.classList.add(css({ display: 'block', padding: '10px 10px 0' }));
-            this.innerHTML = html;
+  connectedCallback() {
+    guilds().then(guilds => {
+      let html = '';
+      guilds.forEach(guild => {
+        const iconCss = css({
+          borderRadius: '50%', userSelect: 'none', transition: '.2s', ':hover': { borderRadius: '.75rem' },
+          width: '48px', height: '48px'
         });
-    }
+        html += `<div class="${css({ marginTop: '4px', marginBottom: '4px' })}">`;
+        html += `<a href="/channels/${guild.id}" class="${css({ ':hover': { textDecoration: 'none' } })}">`;
+        if (!!guild.icon) {
+          html += `<img src="${guildIconURL(guild.id, guild.icon, guild.icon.startsWith('a_'))}" alt="${guild.name}" class="${iconCss}" />`;
+        } else {
+          let iconText = '';
+          guild.name.split(' ').forEach(w => iconText += w[0]);
+          html += `<span class="${iconCss} ${css({
+            color: 'white', display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#36393f',
+            marginBottom: '6px', ':hover': { backgroundColor: '#5865f2' }
+          })}">${iconText}</span>`;
+        }
+        html += '</a></div>';
+      });
+      this.classList.add(css({ display: 'block', padding: '10px 10px 0' }));
+      this.innerHTML = html;
+    });
+  }
 
 }
