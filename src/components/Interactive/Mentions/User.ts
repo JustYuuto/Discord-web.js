b835edde0fb97c5a078780bf713db1c544a073fb
NetherMC <thelio.trinite@outlook.com>
2023-02-03 17:14:37 +0100

    tabs

--- src/components/Interactive/Mentions/User.ts
+++ src/components/Interactive/Mentions/User.ts
@@ -4,16 +4,16 @@ import Component from '../../Component';
 
 export default class UserMention extends Component {
 
-    connectedCallback() {
-        const userId = this.getAttribute('id');
-        user(userId).then(user => {
-            this.classList.add(css({
-                backgroundColor: '#414675', borderRadius: '3px', padding: '0 2px', ':hover': {
-                    backgroundColor: '#5865f2', textDecoration: 'underline'
-                }, cursor: 'pointer', transition: '.1s'
-            }));
-            this.innerHTML = `@${user.username}`;
-        });
-    }
+  connectedCallback() {
+    const userId = this.getAttribute('id');
+    user(userId).then(user => {
+      this.classList.add(css({
+        backgroundColor: '#414675', borderRadius: '3px', padding: '0 2px', ':hover': {
+          backgroundColor: '#5865f2', textDecoration: 'underline'
+        }, cursor: 'pointer', transition: '.1s'
+      }));
+      this.innerHTML = `@${user.username}`;
+    });
+  }
 
 }
