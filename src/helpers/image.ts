bbb7faf6dbfa7590e76089426a2fad1048c4fd1f
JustYuuto <55290516+JustYuuto@users.noreply.github.com>
2023-07-18 13:56:10 +0200

    oops

--- src/helpers/image.ts
+++ src/helpers/image.ts
@@ -15,7 +15,7 @@ export function avatarURL(id: number, avatar: string, discriminator: string, siz
 export function groupAvatarURL(id: number, icon: string, size?: number) {
   return icon ?
     `https://cdn.discordapp.com/channel-icons/${id}/${icon}.png${size ? `?size=${size}` : ''}` : // assets/f7e38ac976a2a696161c923502a8345b.png
-    defaultAvatarURL(0, size);
+    defaultAvatarURL('0', size);
 }
 
 export function avatarImgHTML(url: string, sizes?: number, customCss?: ComponentSelector | SerializedStyles | CSSObject | ArrayCSSInterpolation) {
