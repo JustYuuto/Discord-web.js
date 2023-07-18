3356f0c4329fadb85382871287ee1324f2a7f934
JustYuuto <55290516+JustYuuto@users.noreply.github.com>
2023-07-18 14:19:28 +0200

    added channel name

--- src/components/Channels/Messages.ts
+++ src/components/Channels/Messages.ts
@@ -1,4 +1,4 @@
-import { channelMessages } from '../../helpers/api';
+import { channel, channelMessages } from '../../helpers/api';
 import { css } from '@emotion/css';
 import { urlParts } from '../../helpers/url';
 import { inputCss } from '../Message/Input';
@@ -12,18 +12,25 @@ export default class ChannelMessages extends HTMLElement {
       width: '100%', height: `calc(100vh - ${document.querySelector(`.${inputCss}`)?.scrollHeight || 0}px)`,
       backgroundColor: '#36393f', color: 'white', position: 'relative'
     }));
-    channelMessages(urlParts()[2]).then(messages => {
-      messages = messages.reverse();
-      console.log(`Fetched ${messages.length} messages for channel ${urlParts()[2]}`);
-      html += `<div class="${css({ height: '70vh' })}"></div>`;
-      messages.forEach(message => {
-        html += `<channel-message message="${JSON.stringify(message).replaceAll('"', '&quot;')}"></channel-message>`;
+    html += `<div class="${css({ height: '70vh' })}"></div>`;
+    channel(urlParts()[2]).then((channel) => {
+      html += `<div class="${css({
+        paddingLeft: '16px'
+      })}">`;
+      html += `<h1>Welcome to #${channel.name}</h1>`;
+      html += `</div>`;
+      channelMessages(urlParts()[2]).then(messages => {
+        messages = messages.reverse();
+        console.log(`Fetched ${messages.length} messages for channel ${channel.id}`);
+        messages.forEach(message => {
+          html += `<channel-message message="${JSON.stringify(message).replaceAll('"', '&quot;')}"></channel-message>`;
+        });
+        html += `</div><message-input></message-input>`;
+        this.innerHTML = html;
+        this.querySelector('div')?.scroll(0, 999999);
+      }).catch((e) => {
+        this.innerHTML = `${html}${e}</div><message-input></message-input>`;
       });
-      html += `</div><message-input></message-input>`;
-      this.innerHTML = html;
-      this.querySelector('div')?.scroll(0, 999999);
-    }).catch((e) => {
-      this.innerHTML = `${html}${e}</div><message-input></message-input>`;
     });
   }
 
