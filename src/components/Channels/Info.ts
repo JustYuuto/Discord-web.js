4915a926c87777721c40f9ce5596b8ced930dde7
JustYuuto <55290516+JustYuuto@users.noreply.github.com>
2023-07-19 19:13:30 +0200

    channel info for dms

--- src/components/Channels/Info.ts
+++ src/components/Channels/Info.ts
@@ -1,6 +1,6 @@
 import Component from '../Component';
 import { urlParts } from '../../helpers/url';
-import { Channel, channel, guild } from '../../helpers/api';
+import { Channel, channel, ChannelType, DMChannel, guild } from '../../helpers/api';
 import { css } from '@emotion/css';
 
 export default class ChannelInfo extends Component {
@@ -10,23 +10,40 @@ export default class ChannelInfo extends Component {
       width: '100%', display: 'flex', padding: '20px', height: '58px'
     }));
     let html = '';
-    guild(urlParts()[1]).then(guild => {
+    if (urlParts()[1] !== '@me') {
+      guild(urlParts()[1]).then(guild => {
+        html += `<div class="${css({
+          width: '278px'
+        })}">`;
+        html += `<span>${guild.name}</span>`;
+        html += `</div>`;
+        // @ts-ignore
+        channel(urlParts()[2]).then((channel: Channel) => {
+          html += `<div>`;
+          html += `<span>${channel.name}</span>`;
+          if (channel.topic) {
+            html += `<span>${channel.topic}</span>`;
+          }
+          html += `</div>`;
+          this.innerHTML = html;
+        });
+      });
+    } else {
       html += `<div class="${css({
         width: '278px'
       })}">`;
-      html += `<span>${guild.name}</span>`;
+      html += `<input type="text" class="${css({
+        width: '90%'
+      })}" />`;
       html += `</div>`;
       // @ts-ignore
-      channel(urlParts()[2]).then((channel: Channel) => {
+      channel(urlParts()[2]).then((dm: DMChannel) => {
         html += `<div>`;
-        html += `<span>${channel.name}</span>`;
-        if (channel.topic) {
-          html += `<span>${channel.topic}</span>`;
-        }
+        html += `<span ${dm.type === ChannelType.GROUP_DM && 'contenteditable'}>${dm.name || dm.recipients[0].global_name || dm.recipients[0].display_name || dm.recipients[0].username}</span>`;
         html += `</div>`;
         this.innerHTML = html;
       });
-    });
+    }
   }
 
 }
